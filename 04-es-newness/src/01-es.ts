3085be5f8d3b074d4b784a9817db18475f217a33
Kevin Cunningham <kevin@kevincunningham.co.uk>
2026-02-02 16:03:53 +0000

    adds all my code

---
+++ 04-es-newness/src/01-es.ts
@@ -0,0 +1,73 @@
+// es6 -> es2015
+// es7 -> es2016
+// es8 -> es2017
+// es2018
+
+// 1. Default Parameters
+
+// (a,b) => number
+
+function areaOfRect(length = 1, width = length) {
+  return length * width;
+}
+
+// 2. Template literals
+
+const name = "Ruby";
+const team = "Bitesize Adaptive Learning";
+const role = "Grad";
+
+const summary = `${name} works at ${team} as ${role}.`;
+
+type Greeting = "Hello" | "Bonjour" | "Gutentag"; // Union of types
+let greeting: Greeting = "Hello";
+const farewell = "Goodbye";
+
+type Farewell = "Goodbye" | "Au revoir" | "Tchuss";
+type Target = "World" | "Planet";
+type GreetOptions = `${Greeting} ${Target}`; // Type loops
+type FarewellOptions = `${Farewell} ${Target}`;
+type ConversationEndpoints = GreetOptions | FarewellOptions;
+
+const whatHeSaid: ConversationEndpoints = "Goodbye World";
+
+type Area = "news" | "sport" | "iplayer";
+type ValidUrls = `https://${Area}.bbc.co.uk`;
+type NewUrls = `https://bbc.co.uk/${Area}`;
+type AllUrls = ValidUrls | NewUrls;
+
+// 3. Arrow Functions
+type Operation = (a: number, b: number) => number;
+
+const perimeterOfRect: Operation = (a, b) => a + b; // implicit return
+
+// function -> object
+
+const volumeOfCylinder: Operation = (height, radius) => {
+  const PI = 3.14159;
+  return PI * radius * radius * height;
+};
+
+// 4. var/let/const
+
+function simulateDom() {
+  var pseudoDom = {
+    button1: { click: () => "" },
+    button2: { click: () => "" },
+    button3: { click: () => "" },
+  };
+  // block scope
+  // never use var! use const when can and let when you have to.
+  for (let i = 1; i <= 3; i++) {
+    var element = pseudoDom[`button${i}`];
+    element.click = function () {
+      return `Item ${i} is clicked.`;
+    };
+  }
+
+  console.log(pseudoDom.button1.click()); // {1/1/1} {3/3/3} {1/2/3} {4/4/4}
+  console.log(pseudoDom.button2.click());
+  console.log(pseudoDom.button3.click());
+}
+
+simulateDom();
---
+++ 04-es-newness/src/02-arrays.ts
@@ -0,0 +1,71 @@
+const num: Array<number | string> = [1, 2, 3, 4, 5];
+const num2: (number | string)[] = [1, 4, 9, 16, 25];
+
+const num3 = [...num2];
+const num4 = Array.from(num3);
+num3.push(36);
+
+console.log(num3);
+console.log(num2);
+
+const people = [
+  { name: "Harry", date: new Date() },
+  { name: "Sophie", date: new Date() },
+];
+const people2 = [...people];
+
+console.log(people2);
+console.log(people);
+
+const newPeople = JSON.parse(JSON.stringify(people));
+people2[0].name = "Declan";
+console.log(people);
+console.log(newPeople);
+
+const newerPeople = structuredClone(people);
+console.log(newerPeople);
+
+type UpdatedPerson = {
+  name: string;
+  date: Date;
+  lengthOfName: number;
+  favouriteNumber: number;
+};
+
+type AlmostUpdatedPerson = {
+  name: string;
+  date: Date;
+  lengthOfName: number;
+};
+
+const mappedPeople: UpdatedPerson[] = newerPeople
+  .map<Omit<UpdatedPerson, "favouriteNumber">>((person) => ({
+    ...person,
+    lengthOfName: person.name.length,
+  }))
+  .map<UpdatedPerson>((person) => ({
+    ...person,
+    favouriteNumber: Math.floor(Math.random() * 20),
+  }));
+
+console.log(mappedPeople);
+
+const elem = document.getElementById("header");
+
+elem?.innerText || "No text found.";
+
+elem!.innerText; // I know better - bog off!
+
+if (elem) {
+  elem;
+}
+
+if (!elem) {
+  throw new Error("Element not found");
+}
+
+// 0, '', false, NaN
+elem?.accessKey?.charAt(2) || -1; // optional chaining
+
+// nullish coalescing operator
+elem?.getAttribute?.name ?? -1;
